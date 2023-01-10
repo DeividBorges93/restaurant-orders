ccd1044e2220fef826b4adfa5738bf7a0d5ba44c
DeividBorges93 <deividmborges@gmail.com>
2023-01-10 13:36:45 -0300

    feat(analyse): :sparkles: analyze_log
    adc funções converter log em um dict, filtro por 'custumer', comida mais pedida

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,29 @@
+import csv
+from collections import Counter
+
+
+def log_to_dict(path_to_file, field_names):
+    with open(path_to_file, "r") as file:
+        reader = csv.DictReader(file, fieldnames=field_names)
+        return list(reader)
+
+
+def filter_by_customer(orders, customer):
+    return [order for order in orders if order.get("customer") == customer]
+
+
+def get_most_requested_food(orders):
+    counter = Counter([order.get("food") for order in orders])
+    return counter.most_common(1)[0][0]
+
+
 def analyze_log(path_to_file):
-    raise NotImplementedError
+    orders = log_to_dict(path_to_file, ["customer", "food", "day"])
+
+    maria_orders = filter_by_customer(orders, "maria")
+    maria_consumes = get_most_requested_food(maria_orders)
+
+    with open("data/mkt_campaign.txt", "w") as file:
+        file.write(
+            f"{maria_consumes}\n"
+        )
