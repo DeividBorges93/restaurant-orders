786f08fe3f32700bc603c31830efe8667dbcfcd0
DeividBorges93 <deividmborges@gmail.com>
2023-01-10 13:44:50 -0300

    feat(analyse): :sparkles: adc função que conta quantidade de vezes que a comida foi pedida
    printa resposta da pergunta de arnaldo

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -17,13 +17,21 @@ def get_most_requested_food(orders):
     return counter.most_common(1)[0][0]
 
 
+def count_food_requests(orders, food):
+    return len([order for order in orders if order.get("food") == food])
+
+
 def analyze_log(path_to_file):
     orders = log_to_dict(path_to_file, ["customer", "food", "day"])
 
     maria_orders = filter_by_customer(orders, "maria")
     maria_consumes = get_most_requested_food(maria_orders)
 
+    arnaldo_orders = filter_by_customer(orders, "arnaldo")
+    arnaldo_ask_hamburguer = count_food_requests(arnaldo_orders, "hamburguer")
+
     with open("data/mkt_campaign.txt", "w") as file:
         file.write(
             f"{maria_consumes}\n"
+            f"{arnaldo_ask_hamburguer}\n"
         )
